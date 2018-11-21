b7f0e7ca8573606a12e77746b9cc3b905d19e052
Les Aker <me@lesaker.org>
2018-11-21 14:00:18 -0500

    write out trigger method

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log"
+	"os"
 	"regexp"
+	"time"
 
 	"github.com/akerl/go-lambda/apigw/events"
 	"github.com/akerl/go-lambda/mux"
 	"github.com/akerl/go-lambda/s3"
+	s3api "github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/google/uuid"
+)
 
+const (
+	checkPath = "checks/"
+)
 
 var (
 	triggerRegex = regexp.MustCompile(`^/trigger$`)
@@ -27,6 +37,7 @@ type config struct {
 }
 
 var c config
+var bucket string
 
 func loadConfig() {
 	cf, err := s3.GetConfigFromEnv(&c)
@@ -42,6 +53,7 @@ func loadConfig() {
 
 func main() {
 	loadConfig()
+	bucket = os.Getenv("S3_BUCKET")
 
 	reportRoute := &mux.Route{
 		Path: reportRegex,
@@ -70,14 +82,47 @@ func reportAuthFunc(req events.Request) (events.Response, error) {
 }
 
 func triggerFunc(req events.Request) (events.Response, error) {
+	code := req.QueryStringParameters["code"]
+	if code == "" {
+		return events.Fail("No code provided")
+	}
+
+	if _, err := uuid.Parse(code); err != nil {
+		return events.Fail("Invalid code")
+	}
+
+	s3client, err := s3.Client()
+	if err != nil {
+		return events.Fail("Failed to load S3 client")
+	}
+
+	key := checkPath + code
+
+	data := string(time.Now().Unix())
+
+	input := &s3api.PutObjectInput{
+		Body:   bytes.NewReader([]byte(data)),
+		Bucket: &bucket,
+		Key:    &key,
+	}
+	s3req := s3client.PutObjectRequest(input)
+	_, err = s3req.Send()
+	if err != nil {
+		return events.Fail("Failed to post")
+	}
+	return events.Succeed("Updated!")
 }
 
 func defaultFunc(req events.Request) (events.Response, error) {
-	return events.Redirect("https://"+req.Headers["Host"] + "/report", 303)
+	return events.Redirect("https://"+req.Headers["Host"]+"/report", 303)
 }
 
 func cronFunc(req events.Request) (events.Response, error) {
 }
 
 func cronAuthFunc(req events.Request) (events.Response, error) {
+	if req.RequestContext.AccountID == "" {
+		return events.Response{}, nil
+	}
+	return events.Response{}, fmt.Errorf("request not allowed via API Gateway")
 }
